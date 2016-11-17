d57aef205367a8440b974d402114ee1dd159b1e9
Renaud Pradenc <renaud@ceroce.com>
2016-11-17 14:01:17 +0100

    Produce a first image (a gradient)

--- haskellray.hs
+++ haskellray.hs
@@ -1,3 +1,15 @@
+import Codec.Picture    -- Juicy.Pixels
+
+renderingWidth = 200
+renderingHeight = 100
+
+pixelAtXY :: Int -> Int -> PixelRGB8
+pixelAtXY x y = PixelRGB8  (floor (255.99 * r)) (floor (255.99 * g)) (floor (255.99 * b))
+    where r = (fromIntegral x / fromIntegral renderingWidth)
+          g = (fromIntegral y / fromIntegral renderingHeight)
+          b = 0.2
 main = do
+    let path = "image.png"
+    writePng path $ generateImage pixelAtXY renderingWidth renderingHeight
     putStrLn "Rendering finished"
     return ()
