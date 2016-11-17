0e4e1f77689499a76ebad213e55cb4ecc42a41eb
Renaud Pradenc <renaud@ceroce.com>
2016-11-17 17:48:59 +0100

    The color of a pixel is now stored in a Vec3

--- haskellray.hs
+++ haskellray.hs
@@ -4,16 +4,18 @@ import Vector
 renderingWidth = 200
 renderingHeight = 100
 
+colorAtXY :: Int -> Int -> Vec3
+colorAtXY x y = Vec3 r g b
+    where r = fromIntegral x / fromIntegral renderingWidth
+          g = fromIntegral y / fromIntegral renderingHeight
+          b = 0.2
+
 pixelAtXY :: Int -> Int -> PixelRGB8
 pixelAtXY x y = PixelRGB8  (floor (255.99 * r)) (floor (255.99 * g)) (floor (255.99 * b))
-    where r = (fromIntegral x / fromIntegral renderingWidth)
-          g = (fromIntegral y / fromIntegral renderingHeight)
-          b = 0.2
+    where (Vec3 r g b) = colorAtXY x y
+
 main = do
     let path = "image.png"
-    let v = Vec3 1 2 3
-    let l = len v
-    putStrLn $ "Vector: " ++ show v ++ "length: " ++ show l
     writePng path $ generateImage pixelAtXY renderingWidth renderingHeight
     putStrLn "Rendering finished"
     return ()
